62351e15d566deb5495110bc57f1cb77d55ea602
Omar Terrazas <terrazas.omar@gmail.com>
2025-05-23 14:10:23 -0600

    Add environment variables, update dependencies, and enhance styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import User from "./Pages/User/User";
+
 
 function App() {
   return (
-    <>
-      <h1 className='text-3xl'>XXI Encuentro Regional AIESAD 2025</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/user/:userState/:userHash?" element={<User />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
