09ffa177f7d6e4e819af00a2d46873040bad2d19
Omar Terrazas <terrazas.omar@gmail.com>
2025-05-27 16:45:29 -0600

    Update navigation to use environment variable for root URL in User component

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -39,7 +39,7 @@ function User() {
       cookies.remove("account", { path: "/" });
       cookies.remove("curp", { path: "/" });
       cookies.remove("studyLevel", { path: "/" });
-      navigate("/");
+      navigate(`${import.meta.env.VITE_ROOT_URL}/`);
       return null; 
     } else {
       return <SignIn />;
@@ -81,7 +81,7 @@ function User() {
             Sé parte de este <br /> <span className="text-blue-aiesad">importante encuentro</span> <br /> educativo
           </h4>
           <p className="mt-20 border-b border-b-gray-200 pb-1">
-            <a href="https://cuaed.unam.mx/encuentroaiesad2025/index.html#agenda" className="text-gray-300" rel="noopener noreferrer">Regresar a la agenda</a>
+            <a href={`${import.meta.env.VITE_ROOT_URL}/index.html#agenda`} className="text-gray-300" rel="noopener noreferrer">Regresar a la agenda</a>
           </p>
         </div>
         <div className="bg-gray-100 min-h-screen max-h-max content-center">{userStateForm(userState)}</div>
